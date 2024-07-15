553d83f9093e1b2c40fbf33278240a601318d80b
smoon <sxm4113@gmail.com>
2024-07-15 08:59:53 -0400

    update unit test

--- unittest/unittest.cpp
+++ unittest/unittest.cpp
@@ -1,4 +1,6 @@
 #include "../cpp_image_processing/Image.cpp"
+#include "Local_contrast_enhancement.h"
+
 #include "gtest/gtest.h"
 
 TEST(ImageTest,get_rows)
@@ -12,6 +14,17 @@ TEST(ImageTest,get_cols)
 {
 	Image img{"../image/input.jpg"};
 	const int peri = img.get_cols();
+	EXPECT_EQ(peri,1280);
+
+}
+
+TEST(LocalContrastEnhancementTest,symmetric_boundary)
+{
+	Mat Z = Mat::zeros(10,10);
+	Image img(Z);
+	LocalContrastEnhancement test_obj(img);
+	test_obj.symmetric_boundary
+	const int C.row = img.get_cols();
 	EXPECT_EQ(peri,1280);
 
 }
